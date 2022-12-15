25962261edc2000d61894b3e549c2fa69a5f9e27
Menolo-Homobovanez <118155256+Menolo-Homobovanez@users.noreply.github.com>
2022-12-14 19:25:26 -0500

    Add files via upload

---
+++ website two_12.14/Faq_Scripts/script.js
@@ -0,0 +1,20 @@
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
+
+//stick bar
+
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if(window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
